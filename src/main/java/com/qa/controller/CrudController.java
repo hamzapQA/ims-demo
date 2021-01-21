842d91a39e18387e120dc6284bc5ca1b72e4f227
hamzapQA <hpervez@QA.com>
2021-01-21 12:25:22 +0000

    Updated CRUD controller to take inputs

--- src/main/java/com/qa/controller/CrudController.java
+++ src/main/java/com/qa/controller/CrudController.java
@@ -1,12 +1,18 @@
 package com.qa.controller;
 
+import java.util.List;
+
+/**
+ * Create, Read, Update and Delete controller.
+ * This takes in inputs for each action to be sent to the service class
+ */
 public interface CrudController<T> {
     
-    void readAll();
+    List<T> readAll();
      
-    void create();
+    T create();
      
-    void update();
+    T update();
      
     void delete();
 
