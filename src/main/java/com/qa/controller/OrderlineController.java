dd8857f2ee4d4d0fb9b556aa4463cbc99664d097
hamzapQA <hpervez@QA.com>
2021-01-21 17:58:50 +0000

    Changed import references to fix error

--- src/main/java/com/qa/controller/OrderlineController.java
+++ src/main/java/com/qa/controller/OrderlineController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import com.qa.services.CrudServices;
 import com.qa.controller.CrudController;
+import com.qa.persistence.domain.Orderline;
 import com.qa.utils.Utils;
 
 public class OrderlineController {
