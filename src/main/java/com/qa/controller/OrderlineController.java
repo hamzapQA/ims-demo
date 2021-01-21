a8bf24014462a888c4460b7e4139d19712e6a362
hamzapQA <hpervez@QA.com>
2021-01-21 17:58:01 +0000

    Created OrderLine Controller

---
+++ src/main/java/com/qa/controller/OrderlineController.java
@@ -0,0 +1,11 @@
+package com.qa.controller;
+
+import java.util.List;
+import org.apache.log4j.Logger;
+import com.qa.services.CrudServices;
+import com.qa.controller.CrudController;
+import com.qa.utils.Utils;
+
+public class OrderlineController {
+
+}
