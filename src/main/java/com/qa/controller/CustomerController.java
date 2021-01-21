6f149dc976f46798369f1974b64c0a25dfbf3c49
hamzapQA <hpervez@QA.com>
2021-01-21 12:28:36 +0000

    Updated CRUD functionality in services and controller

--- src/main/java/com/qa/controller/CustomerController.java
+++ src/main/java/com/qa/controller/CustomerController.java
@@ -1,11 +1,17 @@
 package com.qa.controller;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 import com.qa.persistence.domain.Customer;
 import com.qa.services.CrudServices;
 import com.qa.utils.Utils;
 
+/**
+ * Takes in customer details for CRUD functionality
+ *
+ */
 public class CustomerController implements CrudController<Customer>{
 
 	public static final Logger LOGGER = Logger.getLogger(CustomerController.class);
@@ -16,27 +22,61 @@ public class CustomerController implements CrudController<Customer>{
 		this.customerService = customerService;
 	}
 	
-	public void readAll() {
-		for(Customer customer: customerService.readAll()) {
+
+	String getInput() {
+		return Utils.getInput();
+	}
+	
+	/**
+	 * Reads all customers to the logger
+	 */
+	@Override
+	public List<Customer> readAll() {
+		List<Customer> customers = customerService.readAll();
+		for(Customer customer: customers) {
 			LOGGER.info(customer.toString());
 		}
+		return customers;
 	}
 
-	public void create() {
+	/**
+	 * Creates a customer by taking in user input
+	 */
+	@Override
+	public Customer create() {
 		LOGGER.info("Please enter a first name");
-		String firstName = Utils.getInput();
+		String firstName = getInput();
 		LOGGER.info("Please enter a surname");
-		String surname = Utils.getInput();
-		customerService.create(new Customer(firstName, surname));
+		String surname = getInput();
+		Customer customer = customerService.create(new Customer(firstName, surname));
 		LOGGER.info("Customer created");
+		return customer;
 	}
 
-	public void update() {
-		
+	/**
+	 * Updates an existing customer by taking in user input
+	 */
+	@Override
+	public Customer update() {
+		LOGGER.info("Please enter the id of the customer you would like to update");
+		Long id = Long.valueOf(getInput());
+		LOGGER.info("Please enter a first name");
+		String firstName = getInput();
+		LOGGER.info("Please enter a surname");
+		String surname = getInput();
+		Customer customer = customerService.update(new Customer(id, firstName, surname));
+		LOGGER.info("Customer Updated");
+		return customer;
 	}
 
+	/**
+	 * Deletes an existing customer by the id of the customer
+	 */
+	@Override
 	public void delete() {
-		
+		LOGGER.info("Please enter the id of the customer you would like to delete");
+		Long id = Long.valueOf(getInput());
+		customerService.delete(id);
 	}
 	
 }
